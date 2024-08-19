a66e00657a863b23acb127e53ffed6f76cb12651
Eduardo <tkd13000@gmail.com>
2024-08-19 10:02:18 -0600

    Date and time set up

--- scr/time.js
+++ scr/time.js
@@ -1,15 +1,36 @@
 let time = document.getElementById("time");
+let date = document.getElementById("date");
 
 
-function getCurrentTime()
+
+function getCurrentTimeAndDate()
 {
     let now = new Date();//Get current time
 
     let hours = now.getHours();//Get current hour
     let minutes = now.getMinutes();//Get current minutes
 
+    let getMonth = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    let getDay = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+        "Saturday"
+    ];
+
+    
+    let year = now.getFullYear();
+    let month = now.getMonth();
+    let day = now.getDay();
+    let dayOfMonth = now.getDate(); // Obtiene el número del día del mes
+
     time.innerText = hours + ":" + minutes;
+    date.innerText = getDay[day] + ", " + dayOfMonth +" " + getMonth[month] + " " +year;
 }
 
-setInterval(getCurrentTime, 1000);
+
+
+setInterval(getCurrentTimeAndDate, 1000);
 
